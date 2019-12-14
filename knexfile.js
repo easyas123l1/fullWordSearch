83caa0fe95ed1733a20ae9ed991f920b32ecf373
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-14 14:31:55 -0600

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./data/work.db3"
+      filename: "./data/wordsearch.db3"
     },
     migrations: {
       directory: "./data/migrations"

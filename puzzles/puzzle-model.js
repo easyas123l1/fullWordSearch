6810d409213b30520fad5f82bd135ab37650946d
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-01-12 12:44:35 -0600

    add puzzle and words

--- puzzles/puzzle-model.js
+++ puzzles/puzzle-model.js
@@ -1,9 +1,41 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-  getPuzzles
+  getPuzzles,
+  addPuzzle,
+  addWords,
+  getPuzzle,
+  getWords
 };
 
 function getPuzzles() {
   return db("puzzles");
+}
+
+function addPuzzle(data) {
+  return db("puzzles")
+    .insert(data, "id")
+    .then(ids => {
+      const [id] = ids;
+
+      return id;
+    });
+}
+
+function addWords(words) {
+  return db("words")
+    .insert(words, "id")
+    .then(ids => {
+      const [id] = ids;
+
+      return id;
+    });
+}
+
+function getPuzzle(id) {
+  return db("puzzles").where("id", "=", id);
+}
+
+function getWords(id) {
+  return db("words").where("puzzle_id", "=", id);
 }
--- puzzles/puzzle-router.js
+++ puzzles/puzzle-router.js
@@ -14,4 +14,37 @@ router.get("/", (req, res) => {
     });
 });
 
+// add puzzle and words
+router.post("/", (req, res) => {
+  console.log(req.token.user);
+  let { name, code, description, words } = req.body;
+  let newObj = {
+    name,
+    code,
+    description,
+    user_id: req.token.user.id
+  };
+  console.log(newObj);
+  Puzzles.addPuzzle(newObj)
+    .then(id => {
+      for (let word of words) {
+        let newObj2 = {
+          word,
+          puzzle_id: id
+        };
+        Puzzles.addWords(newObj2);
+      }
+      getPuzzle(id).then(puzzle => {
+        Puzzles.getWords(id).then(puzWords => {
+          console.log(puzzle, puzWords);
+          res.status(200).json(puzzle, puzWords);
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to add puzzle" });
+    });
+});
+
 module.exports = router;
